4ae99c58b48702549b9ea87ccbd00d9c9cb1457f
RealHuman-NotAShrew <103269790+RealHuman-NotAShrew@users.noreply.github.com>
2022-04-27 19:39:45 -0400

    Create Spell.java
    A preliminary data structure to hold a spell

---
+++ Spell.java
@@ -0,0 +1,26 @@
+import java.net.URL;
+
+public class Spell
+{
+   private String index;
+   private String name;
+   private String[] desc;
+   private String[] higher_level;
+   private String range;
+   private String[] componenets;
+   private String material;
+   private boolean ritual;
+   private String duration;
+   private boolean concentration;
+   private String casting_time;
+   private int level;
+   private String school;
+   private String[] classes;
+   private String[] subclasses;
+   private URL url;
+   private String attack_type;
+   private String damage_type;
+   private String save_attribute;
+   private String area_of_effect;
+   
+}
