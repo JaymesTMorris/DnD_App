fb67aaa9dd57957f51804e4cc2fd327a85e1d378
Safia Nassiri <niejelandrew@gmail.com>
2022-05-05 17:46:53 -0400

    Update DnDInterface.java

--- DnDInterface.java
+++ DnDInterface.java
@@ -5,23 +5,19 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 
-public class DnDInterface extends Application{
-public static void main (String[] args){
-
-launch(args);
-
-}//end of main
-
-
-public void start(Stage stage) throws Exception
+public class DnDInterface extends Application
 {
+   public static void main (String[] args)
+   {
+      launch(args);
+   }//end of main
 
-Parent root = FXMLLoader.load(getClass().getResource("DnDSpells.fxml"));    
+   public void start(Stage stage) throws Exception
+   {
+      Parent root = FXMLLoader.load(getClass().getResource("DnDSpells.fxml"));    
       Scene scene = new Scene(root);
       stage.setTitle("Demo JavaFX with Scene BUilder");
       stage.setScene(scene);
       stage.show();
-
-}
-
+   }
 }//end of program
